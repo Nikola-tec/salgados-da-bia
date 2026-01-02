4f10d6b8d0f59df5c4424bb5a53e025d71dbbc8a
Nikola-tec <bruno.medeiros@nikola.tec.br>
2026-01-02 16:23:43 -0300

    Atualizações 14.01.26

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -8,7 +8,7 @@ const firebaseConfig = {
   projectId: "salgados-da-bia-31dc0",
   storageBucket: "salgados-da-bia-31dc0.firebasestorage.app",
   messagingSenderId: "2844533583",
-  appId: "1:2844533583:web:0c569024e23ecfbd579b3a"
+  appId: "1:2844533583:web:0c569024e23ecfbd579b3a",
   measurementId: "G-V9X4CFKKGM"
 };
 
