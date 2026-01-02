be483bc860e6058f6bc422880f596e9d663a6bcc
Nikola-tec <bruno.medeiros@nikola.tec.br>
2026-01-02 13:04:02 -0300

    Atualizações 09.01.26

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-// Se der erro na linha abaixo, apague ela e a chamada reportWebVitals() no final
-// import reportWebVitals from './reportWebVitals'; 
+//import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,5 +11,7 @@ root.render(
   </React.StrictMode>
 );
 
-// Se você tiver o arquivo reportWebVitals, descomente a linha abaixo
-// reportWebVitals();
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+//reportWebVitals();
